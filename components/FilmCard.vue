9c620847fecc1a53b1ccecd61f26dd0dd8224c05
khang-2109 <64115215+khang-2109@users.noreply.github.com>
2022-07-12 23:08:33 +0700

    FilmCard Demo

---
+++ components/FilmCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="film-card">
+    <div class="film-card__image">
+      <img :src="img" alt="">
+    </div>
+    <h3 class="film-card__title">{{ film.title }}</h3>
+    <p class="film-card__rating">{{ film.rating }}</p>
+    <p class="film-card__info">
+      <span class="film-card__year">{{ film.year }}</span>
+      <span class="film-card__duration">{{ film.duration }}</span>
+      <span class="film-card__genre">{{ film.genre }}</span>
+    </p>
+    <p class="film-card__description">{{ film.description }}</p>
+    <a class="film-card__comments">{{ film.comments }} comments</a>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    film: {
+      type: Object,
+      required: true
+    },
+    img: {
+      type: String,
+      required: true
+    }
+  },
+  //
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,26 @@
 <template>
-  <h1>Index</h1>
+  <FilmCard :film="this.FilmDetail" :img="this.Img" />
 </template>
 
 <script>
+import FilmCard from '@/components/FilmCard'
 export default {
-  
+  components: {
+    FilmCard
+  },
+  data (){
+    return {
+      FilmDetail: {
+        title: 'Thor: Love and Thunder',
+        rating: '4.9',
+        year: '2022',
+        duration: '',
+        genre: 'Action, Adventure, Fantasy',
+        description: '',
+        comments: '999'
+      },
+      Img: 'https://cdn.galaxycine.vn/media/2022/6/13/1350x900---copy_1655112805440.jpg'
+    }
+  }
 }
 </script>
