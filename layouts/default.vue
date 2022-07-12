0ccb396ccefdce837f9b589eeba19373d5be053f
Huynh Hung <62023702+HuynhHung256@users.noreply.github.com>
2022-07-12 22:00:19 +0700

    update layout

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <NavBar/>
+    <Nuxt/>
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/Layouts/NavBar.vue'
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <Tutorial/>
+  <h1>Index</h1>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  
 }
 </script>
