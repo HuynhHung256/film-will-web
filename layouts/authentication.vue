c5d973453abe5865ac1e79db01be2db13f19a189
Huynh Hung <62023702+HuynhHung256@users.noreply.github.com>
2022-07-22 15:10:33 +0700

    update login page

--- layouts/authentication.vue
+++ layouts/authentication.vue
@@ -1,6 +1,13 @@
 <template>
-<div>
-  <h1>HEHE</h1>
-  <Nuxt/>
-</div>
+  <div
+    id="app"
+    class="bg-fixed bg-cover bg-center h-full"
+    style="
+      background-image: url(https://wamu.org/wp-content/uploads/2021/05/daniele-levis-pelusi-5tWA1mXjTEk-unsplash-1500x1001.jpg);
+    "
+  >
+    <Nuxt />
+  </div>
 </template>
+
+
