d282cbad7208361a78d4b0d840d70dfda850e251
Huynh Hung <62023702+HuynhHung256@users.noreply.github.com>
2022-08-13 22:21:56 +0700

    Update NewsCard.vue

--- components/NewsCard.vue
+++ components/NewsCard.vue
@@ -2,7 +2,7 @@
   <div class="bg-white">
     <div class="max-w-2xl mx-auto  px-4 sm:px-6 lg:max-w-7xl lg:px-8 ">
       <h2 class="sr-only">News</h2>
-        <a href="#" class="group">
+        <NuxtLink to="/news/14" class="group">
           <div class="w-full aspect-[2/1] bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8 shadow shadow-current">
             <img :src=news.img class="w-full h-full object-center object-cover group-hover:opacity-75" />
           </div>
@@ -15,7 +15,7 @@
           <p class="mt-3 text-xs font-medium text-gray-900">
             {{ news.release }} | FILM WILL
           </p>
-        </a>
+        </NuxtLink>
       </div>
     </div>
 
